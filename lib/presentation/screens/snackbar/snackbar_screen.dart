9f6f498972bf46213385da609c792dc0b755a965
Suyai Slootmans <suyaislootmans@gmail.com>
2023-05-31 11:32:55 -0300

    Snackbars

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -4,8 +4,32 @@ class SnakbarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
   const SnakbarScreen({super.key});
 
+  void showCustomSnackBar(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    final snackBar = SnackBar(
+      content: const Text('Hola Mundo'),
+      action: SnackBarAction(
+        label: 'Ok!',
+        onPressed: () {},
+      ),
+      duration: const Duration(seconds: 2),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Snackbars y Diálogos'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => showCustomSnackBar(context),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+        label: const Text('Mostrar Snackbar'),
+      ),
+    );
   }
 }
